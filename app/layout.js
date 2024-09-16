f472c213914b734d3e3afb13111afda6e7bb5bf8
R3h44n <155483352+R3h44n@users.noreply.github.com>
2024-09-17 01:54:39 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,6 @@ export default function RootLayout({ children }) {
             <ToastContainer />
             <Nav />
             {children}
-            <Analytics />
           </FinanceContextProvider>
         </AuthContextProvider> 
       </body>
